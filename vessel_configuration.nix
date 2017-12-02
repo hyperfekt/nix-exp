557bd3ea2d87168d96ae59d83ff2bf057e1b7127
hyperfekt <git@hyperfekt.net>
2017-12-02 20:35:44 +0000

    included osprobe

--- vessel_configuration.nix
+++ vessel_configuration.nix
@@ -4,6 +4,8 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      # Scan system for installed OS and add them to GRUB
+      ./osprobe.nix
     ];
 
   boot.loader.grub.device = "/dev/sda";
