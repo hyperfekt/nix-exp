5dd981bcbc1f0fa49a1ca5a2a17a1c52266e2304
hyperfekt <git@hyperfekt.net>
2017-12-02 20:44:45 +0000

    added bcachefs

--- config/vessel_configuration.nix
+++ config/vessel_configuration.nix
@@ -2,10 +2,9 @@
 
 {
   imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      # Scan system for installed OS and add them to GRUB
-      ./osprobe.nix
+    [ ./hardware-configuration.nix # Include the results of the hardware scan.
+      ./osprobe.nix # Scan system for installed OS and add them to GRUB
+      ./bcachefs-support.nix
     ];
 
   boot.loader.grub.device = "/dev/sda";
