922b06e9bc13fdffb4e8246397c3255a339a9414
hyperfekt <git@hyperfekt.net>
2019-03-09 03:52:04 +0000

    add build script

---
+++ build.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+set -e
+nix-channel --add https://nixos.org/channels/nixos-unstable nixos-unstable
+nix-channel --update
+nix-build '<nixpkgs/nixos>' -A config.system.build.isoImage -I nixos-config="${BASH_SOURCE%/*}/installation-cd-bcachefs-graphical-kde-new-kernel-git-de.nix" --cores 4
