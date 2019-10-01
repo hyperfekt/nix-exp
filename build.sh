5c6279f81fe25c006600fa05da4ab0b131e1a059
hyperfekt <git@hyperfekt.net>
2019-10-01 20:55:33 +0000

    add sudo invocations for channels because 19.09 ISOs don't run under root

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/env bash
 set -e
-nix-channel --add https://nixos.org/channels/nixos-unstable nixos-unstable
-nix-channel --update
-nix-build '<nixpkgs/nixos>' -A config.system.build.isoImage -I nixos-config="${BASH_SOURCE%/*}/installation-cd-bcachefs-graphical-kde-git-de.nix" --cores 4 --show-trace
+sudo nix-channel --add https://nixos.org/channels/nixos-19.09 nixos
+sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos-unstable
+sudo nix-channel --update
+nix-build '<nixpkgs/nixos>' -A config.system.build.isoImage -I nixos-config="${BASH_SOURCE%/*}/installation-cd-bcachefs-graphical-kde-git-de.nix" --cores 4
