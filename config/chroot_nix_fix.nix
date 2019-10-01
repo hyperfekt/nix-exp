4077a3a7be084a752ae1cd93ca48bae0c3fd2acf
hyperfekt <git@hyperfekt.net>
2019-10-01 20:56:07 +0000

    switch nix chroot fix to master instead of patch

--- config/chroot_nix_fix.nix
+++ config/chroot_nix_fix.nix
@@ -1,10 +1,13 @@
 {
   nixpkgs.overlays = [ (
     self: super: {
-      nix = (import <nixos-unstable> {}).nix.overrideAttrs (old: {
-        patches = (old.patched or []) ++ [
-          (builtins.fetchurl "https://github.com/NixOS/nix/commit/06010eaf199005a393f212023ec5e8bc97978537.diff")
-        ];
+      nix = (import <nixos-unstable> {}).nixUnstable.overrideAttrs (old: {
+        src = super.fetchFromGitHub {
+          owner = "nixos";
+          repo = "nix";
+          rev = "4e60c5ec657aeef7973d7383c0aaa113ea10b002";
+          sha256 = "1avmni2xijqwfs5whlpik2s2bhv5v7rbvb7zld40zywcs1l5vxrn";
+        };
       });
     }
   ) ];
