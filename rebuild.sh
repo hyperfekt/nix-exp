5a8f6312499cf23795b5b345cc39675c9308350d
hyperfekt <git@hyperfekt.net>
2020-05-29 16:52:39 +0000

    fix rebuild script

--- rebuild.sh
+++ rebuild.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 resolvconf="$(cat /etc/resolv.conf)";
-nixos-enter -c "echo '$resolvconf' > /etc/resolv.conf; nixos-rebuild boot -I nixpkgs=/cfg/patched:nixos-cfg=/cfg/bismuth.nix --show-trace --option sandbox false" # can't create recursive user namespaces
+nixos-enter -c "echo '$resolvconf' > /etc/resolv.conf; nixos-rebuild boot -I nixpkgs=/cfg/patched -I nixos-config=/cfg/bismuth.nix --show-trace --option sandbox false" # can't create recursive user namespaces
