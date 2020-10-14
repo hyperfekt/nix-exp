3a772388ab35cf85b19910609fe7b38e66fd5080
hyperfekt <git@hyperfekt.net>
2020-10-14 03:11:38 +0000

    update to 20.09

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 set -e
-nix-channel --add https://nixos.org/channels/nixos-20.03 nixos
+nix-channel --add https://nixos.org/channels/nixos-20.09 nixos
 nix-channel --add https://nixos.org/channels/nixos-unstable nixos-unstable
 nix-channel --update
 nix-build '<nixpkgs/nixos>' -A config.system.build.isoImage -I nixos-config="${BASH_SOURCE%/*}/installation-cd-bcachefs-graphical-plasma5-de-git.nix" --cores 4
