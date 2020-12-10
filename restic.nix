0f99961b4344f390ace7986842668b1a5683e0d3
hyperfekt <git@hyperfekt.net>
2020-12-10 21:00:08 +0000

    remove restic

--- restic.nix
+++
@@ -1,2 +0,0 @@
-{ pkgs ? import <nixpkgs> {}}:
-(pkgs.callPackage (builtins.fetchurl "https://raw.githubusercontent.com/NixOS/nixpkgs/b73041d9a6394be6af1667587a1ab8f705ded3ba/pkgs/tools/backup/restic/default.nix") {}).overrideAttrs (old: { doCheck = false; })
