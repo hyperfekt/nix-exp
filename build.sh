1a4ef4ec7c485d25a769237659e92cd0b94ee9fd
hyperfekt <git@hyperfekt.net>
2020-12-10 22:34:27 +0000

    disable sandbox for build.sh

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ set -e
 nix-channel --add https://nixos.org/channels/nixos-20.09 nixos
 nix-channel --add https://nixos.org/channels/nixos-unstable nixos-unstable
 nix-channel --update
-nix-build '<nixpkgs/nixos>' -A config.system.build.isoImage -I nixos-config="${BASH_SOURCE%/*}/installation-cd-bcachefs-graphical-plasma5-de-git.nix" --cores 4
+nix-build '<nixpkgs/nixos>' -A config.system.build.isoImage -I nixos-config="${BASH_SOURCE%/*}/installation-cd-bcachefs-graphical-plasma5-de-git.nix" --cores 4 --option sandbox false
